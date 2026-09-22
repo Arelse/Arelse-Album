9c5f62017900a6f75a8731e0d44dc9a1de6ed5ff
Arelse <verizyves@gmail.com>
2026-09-23 00:04:35 +0530

    Add Sidebar component with navigation links

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default function Sidebar({ open }) {
+  const linkClass = ({ isActive }) => 'nav-link' + (isActive ? ' active' : '')
+  return (
+    <aside className={'sidebar' + (open ? ' open' : '')}>
+      <div className="brand"><span className="brand-dot" /> Arelse Album</div>
+      <NavLink to="/" end className={linkClass}>Dashboard</NavLink>
+      <NavLink to="/albums" className={linkClass}>Albums</NavLink>
+      <NavLink to="/albums?category=Manhwa" className={linkClass}>Manhwa</NavLink>
+      <NavLink to="/albums?category=Memes" className={linkClass}>Memes</NavLink>
+      <NavLink to="/settings" className={linkClass}>Settings</NavLink>
+    </aside>
+  )
+}
